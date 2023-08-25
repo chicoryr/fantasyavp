a086519f323111b71b12a07579e38e14802f2f33
chicoryr <46638417+chicoryr@users.noreply.github.com>
2023-08-25 14:03:59 -0700

    fixing files

---
+++ src/components/TeamRow.tsx
@@ -0,0 +1,14 @@
+import { Team } from "../types/types";
+
+interface TeamDisplayProps {
+    Team: Team;
+  }
+
+export default function TeamRow(Team: TeamDisplayProps) {
+    return(
+        <>
+        <div>{Team.Team.Player1}/{Team.Team.Player2}</div>
+        </>
+    )
+}
+
