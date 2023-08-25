a2771d5abd8145d1c4ce19e958633dd625f4bcc5
chicoryr <46638417+chicoryr@users.noreply.github.com>
2023-08-25 13:56:38 -0700

    add new files, dingus

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Tournament {
+    Team_ID: string[];
+  }
+
+ export interface Team {
+    Player1: string;
+    Player2: string;
+    Player1Rating: number;
+    Player2Rating: number;
+  }
