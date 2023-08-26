512e2b0db5de5dda8b2db0d90e06039df2f56664
Chicory Roth <chicoryroth@gmail.com>
2023-08-25 23:25:50 -0700

    fixed git issue

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,20 @@
+"use client";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/config";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Navbar(){
+    const [user, setUser] = useState(auth.currentUser);
+    const router = useRouter();
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            setUser(user);
+        }else{
+            setUser(null);
+        }
+      });
     return (
         <div className="bg-gray-300 w-full border-b-2 flex items-center justify-between p-2">
             <ul className="flex p-4">
@@ -13,9 +27,24 @@ export default function Navbar(){
             </ul>
             <Link className="text-3xl" href="/">Fantasy AVP</Link>
             <ul className="flex p-4">
+                {user ? <li className="mr-6">
+                    <div className="text-blue-500 hover:text-blue-800 h-full hover:cursor-pointer" onClick={() =>{
+                        signOut(auth).then(() => {
+                            setUser(null);
+                            router.push("/");
+                            alert("You have been signed out.");
+                          }).catch((error) => {
+                            console.log(error);
+                          })
+                    }}>Sign out</div>
+                </li> : <>
                 <li className="mr-6">
-                    <Link className="text-blue-500 hover:text-blue-800 h-full" href="/signup">Signup</Link>
+                    <Link className="text-blue-500 hover:text-blue-800 h-full" href="/signup">Sign up</Link>
                 </li>
+                <li className="mr-6">
+                    <Link className="text-blue-500 hover:text-blue-800 h-full" href="/signin">Sign in</Link>
+                </li>
+                </>}
                 <li className="mr-6">
                     <Link className="text-blue-500 hover:text-blue-800 h-full" href="/about">About</Link>
                 </li>
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../firebase/config"
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const router = useRouter();
+    const handleSignup = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            router.push("/");
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            setError(errorMessage);
+        });
+    }   
+
+
+    return (
+        <div>
+            <h2>Sign in</h2>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form onSubmit={handleSignup}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
+    );
+}
