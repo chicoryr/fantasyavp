cf6b6f41b9f4ad989417b3678a6f384f359ea8d7
Chicory Roth <chicoryroth@gmail.com>
2023-08-27 20:27:45 -0700

    updated formula

--- src/helpers/values.ts
+++ src/helpers/values.ts
@@ -1,15 +1,17 @@
 import { Team } from "../types/types";
 
-function normalize(value: number, srcMin: number, srcMax: number, destMin = 12000, destMax = 250000): number {
-    return destMin + ((value - srcMin) / (srcMax - srcMin)) * (destMax - destMin);
+function normalize(value: number, srcMin: number, srcMax: number, destMin = 12000, destMax = 250000, power = 1.5): number {
+    const normalized = (value - srcMin) / (srcMax - srcMin);
+    const powered = Math.pow(normalized, power);
+
+    return destMin + powered * (destMax - destMin);
 }
 
 export function normalizeTeams(teams: Team[]): number[] {
     const teamSums = teams.map(team => team.Player1Rating + team.Player2Rating);
 
-    const srcMin = Math.min(...teamSums) -1;
+    const srcMin = Math.min(...teamSums) - 1;
     const srcMax = Math.max(...teamSums);
 
     return teamSums.map(sum => normalize(sum, srcMin, srcMax));
 }
-
