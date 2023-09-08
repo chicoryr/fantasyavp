bcc1e3f7b84d11112c8fc61caef31bee4b8d5ac6
chicoryr <46638417+chicoryr@users.noreply.github.com>
2023-09-08 15:33:13 -0700

    fixing loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 "use client"
 import ExplainModal from "@/components/ExplainModal";
 import { UserAuth } from "@/context/AuthContext"
+import { Dna } from "react-loader-spinner";
 
 export default function Home() {
   const {user, loading} = UserAuth();
-  return loading ? <>ADD LOADING SPINNER</> : (user ? <>Logged in!</> : <ExplainModal/>)
+  return loading ? 
+  <div className="grid h-screen place-items-center">
+      <Dna
+      visible={true}
+      height="200"
+      width="200"
+      ariaLabel="dna-loading"
+      wrapperStyle={{}}
+      wrapperClass="dna-wrapper"
+    />
+  </div> 
+: (user ? <div className="grid h-screen place-items-center">Logged in as {user.displayName}</div> : <ExplainModal/>)
 }
