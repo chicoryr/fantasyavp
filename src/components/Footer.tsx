6a91d5f3a96a2b4bbc11f528ef4abae6c84d655e
Chicory Roth <chicoryroth@gmail.com>
2023-09-08 18:08:20 -0700

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className="sticky bottom-0 text-white w-full flex justify-center bg-black">
+      Created by&nbsp;<a href="https://chicoryroth.com" target="_blank" className="underline">Chicory Roth</a>. Source code can be found on&nbsp;<a href="https://github.com/chicoryr/fantasyavp" target="_blank" className="underline">GitHub</a>.
+    </footer>
+  )
+};
